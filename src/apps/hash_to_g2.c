9147eaa65e8afcd0cca6a8adcccddadbdabc8081
kwantam <kwantam@gmail.com>
2019-05-07 16:27:45 -0700

    de-obfuscate init code in apps/

--- src/apps/hash_to_g2.c
+++ src/apps/hash_to_g2.c
@@ -9,8 +9,24 @@
 #include <time.h>
 
 int main(int argc, char **argv) {
-    HASH2_INIT(x, y, z, u1, u2);
+    int retval = 0;
+    const struct cmdline_opts opts = get_cmdline_opts(argc, argv);
+    const bool do_print = opts.test || !opts.quiet;
 
+    curve2_init();
+    mpz_t2 x, y, z, u1, u2;
+    mpz2_inits(x, y, z, u1, u2, (void *)NULL);
+
+    ERR_load_crypto_strings();
+    SHA256_CTX hash_ctx;
+    CHECK_CRYPTO(SHA256_Init(&hash_ctx));
+    EVP_CIPHER_CTX *prng_ctx = EVP_CIPHER_CTX_new();
+    CHECK_CRYPTO(prng_ctx != NULL);
+
+    hash_stdin(&hash_ctx);
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
     for (unsigned i = 0; i < opts.nreps; ++i) {
         next_prng(prng_ctx, &hash_ctx, i);
         next_modp(prng_ctx, u1->s);
@@ -36,6 +52,13 @@ int main(int argc, char **argv) {
             gmp_printf(")\n");
         }
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long elapsed = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    fprintf(opts.quiet ? stdout : stderr, "%ld\n", elapsed);
 
-    HASH2_CLEAR(x, y, z, u1, u2);
+    EVP_CIPHER_CTX_free(prng_ctx);
+    mpz2_clears(x, y, z, u1, u2, (void *)NULL);
+    curve2_uninit();
+
+    return retval;
 }
--- src/util/util.h
+++ src/util/util.h
@@ -32,39 +32,5 @@ void hash_stdin(SHA256_CTX *ctx);
 void next_prng(EVP_CIPHER_CTX *cctx, const SHA256_CTX *hctx, uint32_t idx);
 bool next_modp(EVP_CIPHER_CTX *cctx, mpz_t ret);
 
-// these are ugly, but they let us avoid repeating a bunch of boilerplate in every executable
-#define HASH_INIT_GENERIC(C_INIT_FN, MP_TYPE, MP_INIT_FN, ...)     \
-    int retval = 0;                                                \
-    const struct cmdline_opts opts = get_cmdline_opts(argc, argv); \
-    const bool do_print = opts.test || !opts.quiet;                \
-    C_INIT_FN();                                                   \
-    MP_TYPE __VA_ARGS__;                                           \
-    MP_INIT_FN(__VA_ARGS__, (void *)NULL);                         \
-    ERR_load_crypto_strings();                                     \
-    SHA256_CTX hash_ctx;                                           \
-    CHECK_CRYPTO(SHA256_Init(&hash_ctx));                          \
-    EVP_CIPHER_CTX *prng_ctx = EVP_CIPHER_CTX_new();               \
-    CHECK_CRYPTO(prng_ctx != NULL);                                \
-    hash_stdin(&hash_ctx);                                         \
-    struct timespec start, end;                                    \
-    clock_gettime(CLOCK_MONOTONIC, &start)
-
-#define HASH_CLEAR_GENERIC(C_UNINIT_FN, MP_CLEAR_FN, ...)                                  \
-    clock_gettime(CLOCK_MONOTONIC, &end);                                                  \
-    long elapsed = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec; \
-    fprintf(opts.quiet ? stdout : stderr, "%ld\n", elapsed);                               \
-    EVP_CIPHER_CTX_free(prng_ctx);                                                         \
-    MP_CLEAR_FN(__VA_ARGS__, (void *)NULL);                                                \
-    C_UNINIT_FN();                                                                         \
-    return retval
-
-// for hash_*
-#define HASH_INIT(...) HASH_INIT_GENERIC(curve_init, mpz_t, mpz_inits, __VA_ARGS__)
-#define HASH_CLEAR(...) HASH_CLEAR_GENERIC(curve_uninit, mpz_clears, __VA_ARGS__)
-
-// for hash2_*
-#define HASH2_INIT(...) HASH_INIT_GENERIC(curve2_init, mpz_t2, mpz2_inits, __VA_ARGS__)
-#define HASH2_CLEAR(...) HASH_CLEAR_GENERIC(curve2_uninit, mpz2_clears, __VA_ARGS__)
-
 #define __bls_hash__src__util__util_h__
 #endif  // __bls_hash__src__util__util_h__
