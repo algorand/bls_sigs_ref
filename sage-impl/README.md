83c7f5e4ec61d8b7e4a977957a1715031ccfcac1
kwantam <kwantam@gmail.com>
2019-05-21 17:12:52 -0700

    prep for full BLS impl

--- sage-impl/README.md
+++ sage-impl/README.md
@@ -7,14 +7,6 @@ We implement our optimized variant of the simplified SWU map for hashing to G1 a
 
 # license
 
-All files in the subdirectory other than `utils.py` and `hash_to_base.sage` are
-
 (C) 2019 Riad S. Wahby <rsw@cs.stanford.edu>
 
 See the license in the toplevel directory of this repository.
-
-`utils.py` and `hash_to_base.sage` are from the
-[draft-irtf-cfrg-hash-to-curve](https://github.com/chris-wood/draft-irtf-cfrg-hash-to-curve)
-repository, and subject to that repository's
-[license](https://github.com/chris-wood/draft-irtf-cfrg-hash-to-curve/blob/master/LICENSE.md).
-
--- sage-impl/hash_to_field.py
+++ sage-impl/hash_to_field.py
@@ -6,25 +6,33 @@
 #     https://github.com/pairingwg/bls_standard/blob/master/minutes/spec-v1.md
 
 from hashlib import sha256
+import struct
+import sys
+if sys.version_info[0] != 2:
+    raise RuntimeError("this code is geared toward Python2/Sage, not Python3")
 
 # defined in RFC 3447, section 4.1
 def I2OSP(val, length):
+    val = int(val)
     if val < 0 or val >= (1 << (8 * length)):
         raise ValueError("bad I2OSP call: val=%d length=%d" % (val, length))
     ret = [0] * length
     val_ = val
-    for idx in reversed(range(0, length)):
-        ret[idx] = chr(val_ & 0xff)
+    for idx in reversed(xrange(0, length)):
+        ret[idx] = val_ & 0xff
         val_ = val_ >> 8
-    ret = ''.join(ret)
+    ret = struct.pack("=" + "B" * length, *ret)
+    assert OS2IP(ret, True) == val
     return ret
 
 # defined in RFC 3447, section 4.2
-def OS2IP(octets):
+def OS2IP(octets, skip_assert=False):
     ret = 0
-    for o in octets:
+    for octet in struct.unpack("=" + "B" * len(octets), octets):
         ret = ret << 8
-        ret += ord(o)
+        ret += octet
+    if not skip_assert:
+        assert octets == I2OSP(ret, len(octets))
     return ret
 
 # hash_to_field generates an unbiased element of GF(p^m)
