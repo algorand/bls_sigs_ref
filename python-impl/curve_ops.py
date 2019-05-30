91435f689d2f99377eb59dca50f382d3160923fc
kwantam <kwantam@gmail.com>
2019-05-30 15:23:06 -0700

    python-impl: improve exponentiation routine

--- python-impl/curve_ops.py
+++ python-impl/curve_ops.py
@@ -36,17 +36,25 @@ def from_jacobian(P):
     z3inv = ~(P[2] ** 3)
     return (P[0] * P[2] * z3inv, P[1] * z3inv)
 
-# equality for Jacobian points
-def point_eq(P, Q):
+# point equality or co-z repr
+def _point_eq_coz(P, Q, coZ):
     (X1, Y1, Z1) = P
     (X2, Y2, Z2) = Q
+    inf_match = (Z1 == 0) ^ (Z2 == 0) ^ 1   # true just if both or neither are infty
     Z1sq = pow(Z1, 2)
     Z2sq = pow(Z2, 2)
     X12 = X1 * Z2sq
     X21 = X2 * Z1sq
     Y12 = Y1 * Z2sq * Z2
     Y21 = Y2 * Z1sq * Z1
-    return (X12, Y12) == (X21, Y21)
+    if not coZ:
+        return ((X12, Y12) == (X21, Y21)) & inf_match
+    if not inf_match:
+        raise ValueError("cannot make finite point co-Z with infty")
+    Z12 = Z1 * Z2
+    return ((X12, Y12, Z12), (X21, Y21, Z12))
+point_eq = lambda P, Q: _point_eq_coz(P, Q, False)
+to_coZ = lambda P, Q: _point_eq_coz(P, Q, True)
 
 # http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-2007-bl
 def point_add(P, Q):
@@ -77,7 +85,6 @@ def point_add(P, Q):
 # http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l
 def point_double(P):
     (X, Y, Z) = P
-    p_inf = Z == 0
 
     A = X ** 2
     B = Y ** 2
@@ -105,8 +112,12 @@ def x_chain(P):
             Q = point_double(Q)
     return Q
 
+###
+## Point multiplication routines
+###
+# basic double-and-add
 # NOTE: this routine is NOT constant time!
-def point_mul(k, P):
+def _point_mul_dbladd(k, P):
     ty = type(P[0])
     Q = (ty.zero(p), ty.one(p), ty.zero(p))
     for b in bin(k)[2:]:
@@ -120,6 +131,81 @@ def point_mul(k, P):
             else:
                 Q = point_add(P, Q)
     return Q
+
+# ZDAU', Alg 23 (sans Z-coord) of
+#     Goundar, Joye, Miyaji, Rivain, Venelli, "Scalar multiplication on Weierstrass
+#     elliptic curves from co-Z arithmetic." J Crypt Eng 1(2):161-176, 2011.
+#     http://joye.site88.net/papers/GJMRV11regpm.pdf
+def _zdauP(P, Q):
+    (t1, t2) = P
+    (t4, t5) = Q
+    t6 = t1 - t4
+    t7 = pow(t6, 2)
+    t1 = t1 * t7
+    t4 = t4 * t7
+    t5 = t2 - t5
+    t8 = t1 - t4
+    t2 = 2 * t2 * t8
+    t8 = pow(t5, 2)
+    t4 = t8 - t4 - 2 * t1
+    t6 = pow(t4 + t6, 2) - t7
+    t5 = pow(t5 - t4, 2) - t8 - t2
+    t7 = pow(t4, 2)
+    t5 = t5 - t7
+    t8 = 4 * t7
+    t6 = t6 - t7
+    t6 = t1 * t8
+    t1 = t1 + t4
+    t8 = t8 * t1
+    t7 = t2 + t5
+    t2 = t5 - t2
+    t1 = t8 - t6
+    t5 = t5 * t1
+    t6 = t6 + t8
+    t1 = pow(t2, 2)
+    t1 = t1 - t6
+    t4 = t8 - t1
+    t2 = t2 * t4 - t5
+    t4 = pow(t7, 2) - t6
+    t8 = t8 - t4
+    t7 = t7 * t8
+    t5 = t7 - t5
+    return ((t1, t2), (t4, t5))
+
+def _cneg(P, neg):
+    return (P[0], -P[1]) if neg else P
+
+# left-to-right signed digit co-Z point multiplication, from Algorithm 16 in
+#     Goundar, Joye, Miyaji, Rivain, Venelli, "Scalar multiplication on Weierstrass
+#     elliptic curves from co-Z arithmetic." J Crypt Eng 1(2):161-176, 2011.
+#     http://joye.site88.net/papers/GJMRV11regpm.pdf
+# NOTE: this routine only works for P in the subgroup of order q!
+def point_mul(k, P):
+    # make sure that kz is always 258 bits long and odd
+    kz = (5 * q if k % 2 == 0 else 6 * q) + k
+    assert kz.bit_length() == 258
+    assert kz % 2 == 1
+
+    # initialize: r0 = 3P, r1 = P
+    (r0, r1) = to_coZ(point_add(point_double(P), P), P)
+    assert point_eq(r1, P)
+    # only need X,Y coords
+    (r0, r1) = (r0[:2], r1[:2])
+
+    # left-to-right signed-digit double-and-add
+    bkz = [ 1 if b == '1' else 0 for b in bin(kz)[2:-1] ]
+    for idx in range(1, len(bkz)):
+        r1 = _cneg(r1, bkz[idx] ^ bkz[idx - 1])
+        (r0, r1) = _zdauP(r0, r1)
+
+    # recover z-coordinate
+    # since r1 == +/- P, we know that zP * xP / yP == +/- z1 * x1 / y1
+    # z1 = +/- zP * xP * y1 / (x1 * yP) ; fix up sign using bkz[-1]
+    # clear the denominator of z1 by multiplying through by x1 * yP
+    ((xP, yP, zP), (x1, y1), (x0, y0)) = (P, r1, r0)
+    (z1, z1d) = (zP * xP * y1 * (1 if bkz[-1] else -1), x1 * yP)
+    z1dSq = pow(z1d, 2)
+    return (x0 * z1dSq, y0 * z1dSq * z1d, z1)
 
 ###
 ## Fast cofactor clearing for Ell1
