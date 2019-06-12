56d8c59d31d2a2bcbc12235d93e5a06ec5900aaa
kwantam <kwantam@gmail.com>
2019-06-12 13:25:48 -0700

    rust: move OSSWUMap impls to submodules

--- rust-impl/src/osswu_map/g1.rs
+++ rust-impl/src/osswu_map/g1.rs
@@ -2,8 +2,12 @@
 Constants for OSSWU map for G1
 */
 
-use pairing::bls12_381::transmute::fq;
-use pairing::bls12_381::{Fq, FqRepr};
+use ::chain::chain_pm3div4;
+use ::signum::Signum0;
+use super::{OSSWUMap, osswu_help};
+use ff::Field;
+use pairing::bls12_381::transmute::{fq, g1_projective};
+use pairing::bls12_381::{Fq, FqRepr, G1};
 
 pub(super) const ELLP_A: Fq = unsafe {
     fq(FqRepr([
@@ -27,7 +31,7 @@ pub(super) const ELLP_B: Fq = unsafe {
     ]))
 };
 
-pub(super) const XI: Fq = unsafe {
+const XI: Fq = unsafe {
     fq(FqRepr([
         0x43f5fffffffcaaaeu64,
         0x32b7fff2ed47fffdu64,
@@ -37,3 +41,51 @@ pub(super) const XI: Fq = unsafe {
         0x040ab3263eff0206u64,
     ]))
 };
+
+impl OSSWUMap for G1 {
+    fn osswu_map(u: &Fq) -> G1 {
+        // compute x0 and g(x0)
+        let [usq, xi_usq, _, x0_num, x0_den, gx0_num, gx0_den] =
+            osswu_help(u, &XI, &ELLP_A, &ELLP_B);
+
+        // compute g(X0(u)) ^ ((p - 3) // 4)
+        let sqrt_candidate = {
+            let mut tmp1 = gx0_num;
+            tmp1.mul_assign(&gx0_den); // u * v
+            let mut tmp2 = gx0_den;
+            tmp2.square(); // v^2
+            tmp2.mul_assign(&tmp1); // u * v^3
+            let tmp3 = tmp2;
+            chain_pm3div4(&mut tmp2, &tmp3); // (u v^3) ^ ((p - 3) // 4)
+            tmp2.mul_assign(&tmp1); // u v (u v^3) ^ ((p - 3) // 4)
+            tmp2
+        };
+
+        // select correct values for y and for x numerator
+        let (mut x_num, mut y) = {
+            let mut test_cand = sqrt_candidate;
+            test_cand.square();
+            test_cand.mul_assign(&gx0_den);
+            if test_cand == gx0_num {
+                (x0_num, sqrt_candidate) // g(x0) is square
+            } else {
+                let mut x1_num = x0_num; // g(x1) is square
+                x1_num.mul_assign(&xi_usq); // x1 = xi u^2 g(x0)
+                let mut y1 = usq; // y1 = u^3 g(x0) ^ ((p - 1) // 4)
+                y1.mul_assign(&u);
+                y1.mul_assign(&sqrt_candidate);
+                (x1_num, y1)
+            }
+        };
+
+        // make sure sign of y and sign of u agree
+        let sgn0_y_xor_u = y.sgn0() ^ u.sgn0();
+        y.negate_if(sgn0_y_xor_u);
+
+        // convert to projective
+        x_num.mul_assign(&x0_den); // x_num * x_den / x_den^2 = x_num / x_den
+        y.mul_assign(&gx0_den); // y * x_den^3 / x_den^3 = y
+
+        unsafe { g1_projective(x_num, y, x0_den) }
+    }
+}
--- rust-impl/src/osswu_map/g2.rs
+++ rust-impl/src/osswu_map/g2.rs
@@ -2,8 +2,12 @@
 Constants for OSSWU map for G2
 */
 
-use pairing::bls12_381::transmute::fq;
-use pairing::bls12_381::{Fq2, FqRepr};
+use ::chain::chain_p2m9div16;
+use ::signum::Signum0;
+use super::{OSSWUMap, osswu_help};
+use ff::Field;
+use pairing::bls12_381::transmute::{fq, g2_projective};
+use pairing::bls12_381::{Fq2, FqRepr, G2};
 
 pub(super) const ELLP_A: Fq2 = unsafe {
     Fq2 {
@@ -47,7 +51,7 @@ pub(super) const ELLP_B: Fq2 = unsafe {
     }
 };
 
-pub(super) const XI: Fq2 = unsafe {
+const XI: Fq2 = unsafe {
     Fq2 {
         c0: fq(FqRepr([
             0x760900000002fffdu64,
@@ -68,7 +72,7 @@ pub(super) const XI: Fq2 = unsafe {
     }
 };
 
-pub(super) const ETAS: [Fq2; 4] = unsafe {
+const ETAS: [Fq2; 4] = unsafe {
     [
         Fq2 {
             c0: fq(FqRepr([
@@ -221,3 +225,86 @@ pub(crate) const ROOTS_OF_UNITY: [Fq2; 4] = unsafe {
         },
     ]
 };
+
+impl OSSWUMap for G2 {
+    fn osswu_map(u: &Fq2) -> G2 {
+        // compute x0 and g(x0)
+        let [usq, xi_usq, xi2_u4, x0_num, x0_den, gx0_num, gx0_den] =
+            osswu_help(u, &XI, &ELLP_A, &ELLP_B);
+
+        // compute g(x0(u)) ^ ((p - 9) // 16)
+        let sqrt_candidate = {
+            let mut tmp1 = gx0_den; // v
+            tmp1.square(); // v^2
+            let mut tmp2 = tmp1;
+            tmp1.square(); // v^4
+            tmp2.mul_assign(&tmp1); // v^6
+            tmp2.mul_assign(&gx0_den); // v^7
+            tmp2.mul_assign(&gx0_num); // u v^7
+            tmp1.square(); // v^8
+            tmp1.mul_assign(&tmp2); // u v^15
+            let tmp3 = tmp1;
+            chain_p2m9div16(&mut tmp1, &tmp3); // (u v^15) ^ ((p - 9) // 16)
+            tmp1.mul_assign(&tmp2); // u v^7 (u v^15) ^ ((p - 9) // 16)
+            tmp1
+        };
+
+        for root in &ROOTS_OF_UNITY[..] {
+            let mut y0 = *root;
+            y0.mul_assign(&sqrt_candidate);
+
+            let mut tmp = y0;
+            tmp.square();
+            tmp.mul_assign(&gx0_den);
+            if tmp == gx0_num {
+                let sgn0_y_xor_u = y0.sgn0() ^ u.sgn0();
+                y0.negate_if(sgn0_y_xor_u);
+                y0.mul_assign(&gx0_den); // y * x0_den^3 / x0_den^3 = y
+
+                tmp = x0_num;
+                tmp.mul_assign(&x0_den); // x0_num * x0_den / x0_den^2 = x0_num / x0_den
+
+                return unsafe { g2_projective(tmp, y0, x0_den) };
+            }
+        }
+
+        // If we've gotten here, g(X0(u)) is not square. Use X1 instead.
+        let x1_num = {
+            let mut tmp = x0_num;
+            tmp.mul_assign(&xi_usq);
+            tmp
+        };
+        let gx1_num = {
+            let mut tmp = xi2_u4;
+            tmp.mul_assign(&xi_usq); // xi^3 u^6
+            tmp.mul_assign(&gx0_num);
+            tmp
+        };
+        let sqrt_candidate = {
+            let mut tmp = sqrt_candidate;
+            tmp.mul_assign(&usq);
+            tmp.mul_assign(u);
+            tmp
+        };
+        for eta in &ETAS[..] {
+            let mut y1 = *eta;
+            y1.mul_assign(&sqrt_candidate);
+
+            let mut tmp = y1;
+            tmp.square();
+            tmp.mul_assign(&gx0_den);
+            if tmp == gx1_num {
+                let sgn0_y_xor_u = y1.sgn0() ^ u.sgn0();
+                y1.negate_if(sgn0_y_xor_u);
+                y1.mul_assign(&gx0_den); // y * x0_den^3 / x0_den^3 = y
+
+                tmp = x1_num;
+                tmp.mul_assign(&x0_den); // x1_num * x0_den / x0_den^2 = x1_num / x0_den
+
+                return unsafe { g2_projective(tmp, y1, x0_den) };
+            }
+        }
+
+        panic!("Failed to find square root in G2 osswu_map");
+    }
+}
